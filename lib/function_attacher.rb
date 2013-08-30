430cb99c98862f0c4427789db5034486fc5cccc8
Kalle Lindström <lindstrom.kalle@gmail.com>
2013-08-30 20:42:34 +0200

    Change strings to symbols

--- lib/function_attacher.rb
+++ lib/function_attacher.rb
@@ -9,13 +9,13 @@ module AutoItFFI
     #
     def self.attach(lib)
 
-      lib.attach_function("AU3_MouseGetPosX", "AU3_MouseGetPosX", [], :int)
-      lib.attach_function("AU3_MouseGetPosY", "AU3_MouseGetPosY", [], :int)
-      lib.attach_function("AU3_IsAdmin", "AU3_IsAdmin", [], :int)
-      lib.attach_function("AU3_MouseMove", "AU3_MouseMove", [:long, :long, :long], :long)
+      lib.attach_function :AU3_MouseGetPosX, [], :int
+      lib.attach_function :AU3_MouseGetPosY, [], :int
+      lib.attach_function :AU3_IsAdmin, [], :int
+      lib.attach_function :AU3_MouseMove, [:long, :long, :long], :long
 
-      lib.attach_function("AU3_WinMinimizeAll", "AU3_WinMinimizeAll", [], :void)
-      lib.attach_function("AU3_WinMinimizeAllUndo", "AU3_WinMinimizeAllUndo", [], :void)
+      lib.attach_function :AU3_WinMinimizeAll, [], :void
+      lib.attach_function :AU3_WinMinimizeAllUndo, [], :void
 
     end
 
